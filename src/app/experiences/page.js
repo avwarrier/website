e0a49c5f1814f4d937d272a375a489ea4f6d9ac3
avwarrier <avwarrier@ucdavis.edu>
2024-08-17 16:44:01 -0700

    experiences done

--- src/app/experiences/page.js
+++ src/app/experiences/page.js
@@ -1,10 +1,73 @@
 import React from 'react'
+import ibm_logo from '../../assets/ibm_logo.jpeg'
+import clocky_logo from '../../assets/toymail_co_logo.jpeg'
+import Experience from '@/components/Experience/Experience'
 
 const Experiences = () => {
+  /*
+  Reformatted Deferred Defects system for the Platform Dashboard by adding new fields (graphs and tables) for customer facing issues and updated backend code to filter data by new parameters
+Created new APIs to handle data on the platform’s Cluster Portal and wrote new SQL queries to filter clusters and virtual machines by multiple parameters including addons, squad, and versions
+Wrote and updated python scripts to populate active cluster data and added a new system to access quotas for cluster use sorted by squad, which was used to update data summary graphs on the portal
+Developed new data schemas and tables in PostgreSQL database to better organize cluster information and increase efficiency, and automated table updating in the cluster python scripts
+*/
+
+/*
+Worked on updating and developing functions in Python to find counterfeit products across multiple websites such as Walmart, Amazon, Ebay, Google, Alibaba, Mercari, etc.
+Used web scraping libraries to parse site data and grab images, used cv2 to compare fake images and record them based on match parameters, and sent data to frontend using Django
+Developed frontend screens to display counterfeits and updated website using Shopify (liquid)
+
+*/
+
+  const experiences = [
+    {
+      company: "IBM",
+      position: "Software Engineer Intern",
+      start: "June 2024",
+      end: "Dec 2024",
+      location: "San Jose, CA",
+      teamName: "Platform Engineering (Cloud Pak for Data)",
+      description: [
+        "Reformatted Deferred Defects system for the Platform Dashboard by adding new fields (graphs and tables) for customer facing issues and updated backend code to filter data by new parameters",
+        "Created new APIs to handle data on the platform’s Cluster Portal and wrote new SQL queries to filter clusters and virtual machines by multiple parameters including addons, squad, and versions",
+        "Wrote and updated python scripts to populate active cluster data and added a new system to access quotas for cluster use sorted by squad, which was used to update data summary graphs on the portal",
+        "Developed new data schemas and tables in PostgreSQL database to better organize cluster information and increase efficiency, and automated table updating in the cluster python scripts"
+      ],
+      skills: "Python, React.js, Node.js, PostgreSQL, Javascript, Backend Dev, Full-Stack Dev, Automation",
+      color: "#0043ce",
+      logo: ibm_logo,
+    },
+    {
+      company: "Clocky LLC",
+      position: "Software Developer Intern",
+      start: "June 2023",
+      end: "Sept 2023",
+      location: "Remote",
+      teamName: null,
+      description: [
+        "Worked on updating and developing functions in Python to find counterfeit products across multiple websites such as Walmart, Amazon, Ebay, Google, Alibaba, Mercari, etc.",
+        "Used web scraping libraries to parse site data and grab images, used cv2 to compare fake images and record them based on match parameters, and sent data to frontend using Django",
+        "Developed frontend screens to display counterfeits and updated website using Shopify (liquid)"
+      ],
+      skills: "Python, Django, Selenium, Web Scraping Libraries - Beautiful Soup, webDriver, Liquid, Shopify",
+      color: "#0043ce",
+      logo: clocky_logo,
+    }
+  ]
+
   return (
-    <div className="flex w-screen justify-center h-[90vh] gap-[70px]">
-      <div className='mt-[10vh]'>
-        Experiences
+    <div className="flex w-screen justify-center h-[90vh] gap-[100px]">
+      
+      <div className='mt-[12vh] flex flex-col items-center'>
+        <div className='w-[20px] h-[20px] rounded-full border-[6px] border-black'></div>
+        <div className='h-[400px] mt-[-4px] mb-[-4px] w-[6px] bg-black'></div>
+        <div className='w-[20px] h-[20px] rounded-full border-[6px] border-black'></div>
+      </div>
+      <div className='mt-[7vh] flex flex-col gap-[30px]'>
+        {
+          experiences.map((experience) => (
+            <Experience experience={experience} />
+          ))
+        }
       </div>
     </div>
   )
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,8 +1,32 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Experience = () => {
+const Experience = ({ experience }) => {
   return (
-    <div>Experience</div>
+    <div className='flex items-start gap-[20px] w-[1000px]'>
+        <div className='w-[150px] h-[200px]'>
+            <Image src={experience.logo} alt='company logo' className='rounded-md'/>
+        </div>
+        <div className='flex flex-col justify-center'>
+            <h2 className='font-semibold text-[28px]'>{`${experience.position} @ ${experience.company}`}</h2>
+            <div className='flex items-center gap-[10px]'>
+                <p className='text-[17px]'>{`${experience.start} - ${experience.end}`}</p>
+                <p className='text-[17px]'>|</p>
+                <p className='text-[17px]'>{`${experience.location}`}</p>
+            </div>
+            {experience.teamName && <p className='text-[18px] font-medium mt-[5px]'>{`${experience.teamName}`}</p>}
+            <div className=' w-[800px] flex flex-col gap-[10px] mt-[10px] ml-[5px]'>
+                {
+                    experience.description.map((desc, index) => (
+                        <div key={index} className='flex gap-[5px] items-start'>
+                            <p>-</p>
+                            <p>{desc}</p>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
   )
 }
 
